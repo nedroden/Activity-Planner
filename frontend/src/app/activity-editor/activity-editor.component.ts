c6ec24d18fedad681f6ca6b66321fb8d07e7a2a3
Robert Monden <dev@robertmonden.com>
2018-06-24 20:58:23 +0200

    Forms can no longer be submitted if input is invalid

--- frontend/src/app/activity-editor/activity-editor.component.ts
+++ frontend/src/app/activity-editor/activity-editor.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Activity } from '../activity';
 import { ActivityService } from '../activity.service';
 import { ActivatedRoute, Router } from '@angular/router';
-import { FormBuilder, Validators, FormControl } from '@angular/forms';
+import { FormGroup, Validators, FormControl } from '@angular/forms';
 
 import { two_digits, trigger_notification } from '../../util';
 import { environment } from '../../environments/environment';
@@ -22,33 +22,32 @@ export class ActivityEditorComponent implements OnInit {
 
     constructor(private _activityService: ActivityService,
                 private _activatedRoute: ActivatedRoute,
-                private _router: Router,
-                private _formBuilder: FormBuilder) {
+                private _router: Router) {
         this.id = -1;
         this.activity = new Activity;
 
-        this.editorForm = this._formBuilder.group({
-            title: new FormControl(this.activity.title, Validators.compose([
+        this.editorForm = new FormGroup({
+            'title': new FormControl(this.activity.title, Validators.compose([
                 Validators.required,
                 Validators.minLength(5)
             ])),
-            location: new FormControl(this.activity.location, Validators.compose([
+            'location': new FormControl(this.activity.location, Validators.compose([
                 Validators.maxLength(50)
             ])),
-            description: new FormControl(this.activity.description, Validators.compose([
+            'description': new FormControl(this.activity.description, Validators.compose([
                 Validators.maxLength(500)
             ])),
-            startDateInput: new FormControl(this.activity.start_date_value, Validators.compose([
+            'startDate': new FormControl(this.activity.start_date_value, Validators.compose([
                 Validators.required
             ])),
-            startTimeInput: new FormControl(this.activity.start_time_value, Validators.compose([
+            'startTime': new FormControl(this.activity.start_time_value, Validators.compose([
                 Validators.required,
                 Validators.pattern('([0-6][0-9])\:([0-6][0-9])')
             ])),
-            endDateInput: new FormControl(this.activity.end_date_value, Validators.compose([
+            'endDate': new FormControl(this.activity.end_date_value, Validators.compose([
                 Validators.required
             ])),
-            endTimeInput: new FormControl(this.activity.end_time_value, Validators.compose([
+            'endTime': new FormControl(this.activity.end_time_value, Validators.compose([
                 Validators.required,
                 Validators.pattern('([0-6][0-9])\:([0-6][0-9])')
             ]))
@@ -75,6 +74,14 @@ export class ActivityEditorComponent implements OnInit {
                 activity.end_time_value = two_digits(endsAt.getHours()) + ':' + two_digits(endsAt.getMinutes());
 
                 this.activity = activity;
+                this.editorForm.patchValue(this.activity);
+
+                this.editorForm.patchValue({
+                    startDate: activity.start_date_value,
+                    startTime: activity.start_time_value,
+                    endDate: activity.end_date_value,
+                    endTime: activity.end_time_value
+                });
             });
         }
 
@@ -84,29 +91,20 @@ export class ActivityEditorComponent implements OnInit {
         });
     }
 
-    isValid(): boolean {
-        for (let control of this.editorForm)
-            if (control.invalid)
-                return false;
-
-        return true;
-    }
-
     saveActivity(e): void {
-        if (this.isValid()) {
-            let fields = ['title', 'location', 'startingDate', 'startingTime', 'endDate', 'endTime', 'description', 'attachments'];
-            this._activityService.update(this.id, fields,
-                result => {
-                    if (result === null || Object.keys(result).length === 0)
-                        trigger_notification('error', 'Could not save activity', 4000, 'danger');
-                    else
-                        this._router.navigateByUrl('/activity/' + result.id);
-                },
-                error => trigger_notification('error', 'Could not save activity')
-            );
-        }
-        else
-            trigger_notification('error', 'Please fix all errors before submitting the form.', 4000, 'danger');
+        if (this.editorForm.status === 'INVALID')
+            return;
+
+        let fields = ['title', 'location', 'startingDate', 'startingTime', 'endDate', 'endTime', 'description', 'attachments'];
+        this._activityService.update(this.id, fields,
+            result => {
+                if (result === null || Object.keys(result).length === 0)
+                    trigger_notification('error', 'Could not save activity', 4000, 'danger');
+                else
+                    this._router.navigateByUrl('/activity/' + result.id);
+            },
+            error => trigger_notification('error', 'Could not save activity')
+        );
     }
 
     deleteAttachment(id: number): void {
